24f91d3a0a205f48785bff2e93fb9396bdaaad21
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2017-06-02 00:43:30 +0900

    app run fixed

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,8 +1,7 @@
 'use-strict';
 
 var app = angular.module('TestieApp', ['ui.router', 'ngRoute'])
-app.preventDefault()
-	app.run(function($state, $rootScope) {
+	.run(function($state, $rootScope) {
 		alert("Running!");
 	    $state.go('home'); 
 	});
