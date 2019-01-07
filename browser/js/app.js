9ea1663237253fc80181f27aca879ed26d549688
Se Choong Kim <sck324@nyu.edu>
2019-01-07 15:53:26 -0500

    added bootstrap and columns and home state

--- browser/js/app.js
+++ browser/js/app.js
@@ -7,3 +7,16 @@ var app = angular.module('BeetsApp', ['fsaPreBuilt','ui.router', 'ngRoute'])
 app.factory('moment', function ($window) {
         return $window.moment;
     });
+
+var bootstrap_enabled = (typeof $().emulateTransitionEnd == 'function');
+console.log(bootstrap_enabled)
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider.state('somethinghere', {
+        url: '/other',
+        controller: 'HomeController',
+        templateUrl: 'js/home/about.html'
+    });
+});
